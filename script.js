c60094fc7c7c4fb590231b424c1b9140e08b3b89
mayur061002 <mayurmakode131@gmail.com>
2024-07-24 19:59:31 +0530

    progress3

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+function locoscroll(){
+    gsap.registerPlugin(ScrollTrigger);
+
+    const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+    });
+
+    locoScroll.on("scroll", ScrollTrigger.update);
+
+    ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+    ScrollTrigger.refresh();
+
+}
+locoscroll();
+
 function cursorEffect(){
     var page1content = document.querySelector("#page1-content");
     var cursor = document.querySelector("#cursor");
@@ -27,6 +54,23 @@ function cursorEffect(){
             opacity: 0
         })
     });
-};
-
+}
 cursorEffect();
+
+function page2animation(){
+    gsap.from("#middle h2", {
+        y: 120,
+        stagger: 0.2,
+        duration: 1,
+        srcollTrigger: {
+            trigger: "#page2",
+            scroller: "#main",
+            start: "top 40%",
+            end: "top 37%",
+            markers: true,
+            scrub: 2
+        }
+    })
+}
+
+page2animation();
