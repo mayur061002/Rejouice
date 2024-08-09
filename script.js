dac369a61e34bf23fb811955124ee6396dc3f34f
mayur061002 <mayurmakode131@gmail.com>
2024-08-09 14:49:03 +0530

    all pages prob solved for responsive

--- script.js
+++ script.js
@@ -2,21 +2,21 @@ function locoscroll(){
     gsap.registerPlugin(ScrollTrigger);
 
     const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true
+    el: document.querySelector("#main"),
+    smooth: true
     });
 
     locoScroll.on("scroll", ScrollTrigger.update);
 
     ScrollTrigger.scrollerProxy("#main", {
-  scrollTop(value) {
+    scrollTop(value) {
     return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
+    }, 
+    getBoundingClientRect() {
     return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    },
+
+    pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
     });
 
     ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
@@ -75,6 +75,12 @@ function page2animation() {
         }
     });
 
+}
+page2animation();
+
+
+function page3animation(){
+
     gsap.from("#middle h2", {
         x: 120,
         stagger: 0.2,
@@ -90,7 +96,7 @@ function page2animation() {
     });
 
 }
-page2animation();
+page3animation()
 
 
 function Swiperjs(){
